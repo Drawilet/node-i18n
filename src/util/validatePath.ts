f82a55395fe862e4f644460685ca41316f8bc7dd
Drawilet <drawiletdev@gmail.com>
2023-11-13 19:10:40 -0600

    Add support for writing data to a new file path.

--- src/util/validatePath.ts
+++ src/util/validatePath.ts
@@ -1,7 +1,11 @@
 import { existsSync, mkdirSync, writeFileSync } from "fs";
+import { dirname } from "path";
 
 export const validatePath = (path: string, data?: any) => {
-  if (!existsSync(path))
-    if (data) writeFileSync(path, data);
-    else mkdirSync(path);
+  if (existsSync(path)) return;
+
+  if (data) {
+    mkdirSync(dirname(path), { recursive: true });
+    writeFileSync(path, data, { encoding: "utf-8" });
+  } else mkdirSync(path, { recursive: true });
 };
