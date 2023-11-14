022534c2a141ab9650335497acbcd4a534815dd9
Drawilet <drawiletdev@gmail.com>
2023-11-13 19:11:34 -0600

    test: add config paths

--- test/index.ts
+++ test/index.ts
@@ -4,8 +4,9 @@ import GoogleStrategy from "../src/strategies/Google";
 const i18n = new I18n(new GoogleStrategy("http://212.107.31.118:80"), {
   locales: ["es", "en"],
   defaultLocale: "en",
-  dir: __dirname + "/i18n",
   files: __dirname + "/routes",
+  data_path: __dirname + "/i18n/routes.json",
+  cache_path: __dirname + "/cache/i18n.json",
 });
 
 (async () => {
