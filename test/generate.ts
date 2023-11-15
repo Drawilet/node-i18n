e52c5f2840a2d52f382fe2e80912727ad8814687
Drawilet <drawiletdev@gmail.com>
2023-11-14 19:45:35 -0600

    test: add config support

---
+++ test/generate.ts
@@ -0,0 +1,4 @@
+import I18nGenerator from "../src/generator";
+const i18nGenerator = new I18nGenerator();
+
+i18nGenerator.generate();
--- test/index.ts
+++ test/index.ts
@@ -1,16 +1,7 @@
 import I18n from "../src/";
-import GoogleStrategy from "../src/strategies/Google";
-
-const i18n = new I18n(new GoogleStrategy("http://212.107.31.118:80"), {
-  locales: ["es", "en"],
-  defaultLocale: "en",
-  files: __dirname + "/routes",
-  data_path: __dirname + "/i18n/routes.json",
-  cache_path: __dirname + "/cache/i18n.json",
-});
+const i18n = new I18n();
 
 (async () => {
-  await i18n.generate();
   await i18n.load();
 
   const client = i18n.createClient("es", "/customers/");
