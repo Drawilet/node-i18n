fec368ef3c588f26c4fd447d715015b75e5fed1c
Drawilet <drawiletdev@gmail.com>
2023-11-27 20:39:55 -0600

    test:  multiple inputs support

--- i18n.config.js
+++ i18n.config.js
@@ -1,10 +1,15 @@
 const GoogleStrategy = require("./dist/strategies/Google").default;
 
+/**@type {import("./types/I18nConfig").default*/
 module.exports = {
   strategy: new GoogleStrategy("http://212.107.31.118:80"),
   locales: ["es", "en"],
   defaultLocale: "en",
-  input_path: __dirname + "/test/routes",
-  output_path: __dirname + "/test/locales/routes.json",
+
+  output_path: __dirname + "/test/locales/",
   cache_path: __dirname + "/test/cache/i18n.json",
+
+  output_mode: "separated",
+
+  inputs: [__dirname + "/test/routes", __dirname + "/test/components"],
 };
